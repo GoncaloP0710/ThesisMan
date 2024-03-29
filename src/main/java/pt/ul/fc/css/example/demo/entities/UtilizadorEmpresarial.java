94079de747c8357077c85c85cadeb1265e2f8ae1
M2B06x2-2E3 <jose.bras15@hotmail.com>
2024-03-29 16:13:28 +0000

    Overriden object methods in UtilizadorEmpresarial

--- src/main/java/pt/ul/fc/css/example/demo/entities/UtilizadorEmpresarial.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/UtilizadorEmpresarial.java
@@ -1,6 +1,7 @@
 package pt.ul.fc.css.example.demo.entities;
 
 import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
@@ -13,22 +14,51 @@ import jakarta.persistence.Id;
 public class UtilizadorEmpresarial {
     
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    int id;
-    String empresa;
+    private Integer id;
+    private String empresa;
     @ElementCollection
     @Column(name = "temas_propostos")
-    List<Tema> temasPropostos;
+    private List<Tema> temasPropostos;
 
     public UtilizadorEmpresarial(String empresa, List<Tema> temas) {
         this.empresa = empresa;
         temasPropostos = temas;
     }
 
-    public getEmpresa() {
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEmpresa() {
         return empresa;
     }
 
-    public getTemasPropostos() {
+    public List<Tema> getTemasPropostos() {
         return temasPropostos;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        var that = (UtilizadorEmpresarial) obj;
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.empresa, that.empresa) &&
+                Objects.equals(this.temasPropostos, that.temasPropostos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, empresa, temasPropostos);
+    }
+
+    @Override
+    public String toString() {
+        return "Utilizador Empresarial[" +
+                "id=" + id + ", " +
+                "empresa=" + empresa + ", " +
+                "temas propostos=" + temasPropostos + ']';
     }
 }
