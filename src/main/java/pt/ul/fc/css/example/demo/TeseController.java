d84c195bb1a90e08f2138802b0b144399b883735
Gonçalo Pinto <fc58178@alunos.fc.ul.pt>
2024-03-30 15:49:52 +0000

    TeseController + alteracao no TeseRepository

---
+++ src/main/java/pt/ul/fc/css/example/demo/TeseController.java
@@ -0,0 +1,36 @@
+package pt.ul.fc.css.example.demo;
+
+import java.util.List;
+
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import pt.ul.fc.css.example.demo.entities.Tese;
+import pt.ul.fc.css.example.demo.repositories.TeseRepository;
+
+@RestController
+@RequestMapping("/api")
+public class TeseController {
+    private final TeseRepository teseRepository;
+
+    public TeseController(TeseRepository teseRepository) {
+        this.teseRepository = teseRepository;
+    }
+
+    @GetMapping("/tese/{id}")
+    public Tese getTese(@PathVariable Integer id) {
+        return this.teseRepository.getReferenceById(id);
+    }
+
+    @PutMapping("/tese")
+    public Long putTese(@NonNull @RequestBody Tese tese) {
+        Tese saved = teseRepository.save(tese);
+        return saved.getId();
+    }
+
+    @RequestMapping(value = "/index")
+    public ModelAndView home() {
+        return new ModelAndView("index");
+    }
+}
